dda603e9d5b95b3533bf0748c3dd83eaf8262f2f
examjoke <jokernazi10@gmail.com>
2025-11-10 09:15:27 +0530

    Delete Program10.php

--- Program10.php
+++
@@ -1,8 +0,0 @@
-<?php
-$arr=array(1,2,2,3,4,4,4,5,6,6);
-echo"original sorted List:,br>";
-print_r($arr);
-$result=array_unique($arr);
-echo",br><br>List after removing duplicates:<br>";
-print_r($result);
-?>
